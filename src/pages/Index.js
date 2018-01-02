af4981642b8647ee45f9bfb858e38fed70a0d741
Sam Maaß <sam@maass.co>
2018-01-02 14:12:19 +0100

    add landing page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,38 +1,72 @@
 import React from 'react';
-import { Container, Heading3, FlowText, FlexRow } from './../style-components';
-import { Link } from 'react-router-dom';
-import TripTile from '../components/TripTile';
+import Typography from 'material-ui/Typography';
+import Card, { CardContent } from 'material-ui/Card';
 
-const Home = () => {
+const containerStyle = {
+  gridColumn: 2,
+  gridRow: 2
+};
+
+const gridStyle = {
+  backgroundImage: 'url("/landing.jpg")',
+  backgroundSize: 'cover',
+  display: 'grid',
+  gridGap: 32,
+  minHeight: '100vh',
+  gridTemplateColumns: '1fr minmax(auto,800px) 1fr',
+  gridTemplateRows: '1fr 400px 1fr',
+  backgroundPosition: '50%'
+};
+
+const logoStyle = {
+  marginTop: 10,
+  width: '80%',
+  maxWidth: 400,
+  justifySelf: 'center',
+  gridColumn: 2,
+  gridRow: 1,
+  backgroundImage: 'url("/logo.png")',
+  backgroundSize: 'contain',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '50%'
+};
+
+const listStyle = {
+  listStyle: 'none',
+  paddingLeft: 10
+};
+
+const Index = () => {
   return (
-    <div className="index_page">
-      <Container>
-        <Heading3>Flystr watches flight prices, you relax</Heading3>
-        <FlowText>
-          Stop wasting your time crawling blogs and flight search engines for
-          your next trip. Just enter places you want to go visit and get
-          notified when a deal pops up.
-        </FlowText>
-        <FlexRow>
-          <TripTile
-            name="Europe-Trip"
-            duration="12 days"
-            season="Summer 2018"
-            priceLimit="<500 €"
-            status="active"
-          />
-          <TripTile
-            name="Europe-Trip"
-            duration="12 days"
-            season="Summer 2018"
-            priceLimit="<500 €"
-            status="active"
-          />
-        </FlexRow>
-        <Link to="/new-trip"> Add your own trip </Link>
-      </Container>
+    <div className="grid" style={gridStyle}>
+      <div className="logo" style={logoStyle} />
+      <div className="container" style={containerStyle}>
+        <Card>
+          <CardContent>
+            <Typography type="headline" component="h2">
+              Easy & cheap flights
+            </Typography>
+            <br />
+            <Typography component="p">
+              Travelagencies will always make you overpay, because they need to
+              make a living of your money. And Flight Search Engines only give
+              you a snapshot of whats happening, but prices change all the time.
+              Flystr is here to make frugal travel easy again.
+              <ul style={listStyle}>
+                <li> 🌍 Add all your destinations</li>
+                <li> 🤖 Let our bot do the searching</li>
+                <li> 📨 Get notified when a good deal comes up</li>
+                <li> 🛫 Save money, go everywhere</li>
+              </ul>
+              <b>No more searching, no more overpaying.</b>
+              <br />
+              <i>Coming soon.</i>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
 
-export default Home;
+export default Index;
