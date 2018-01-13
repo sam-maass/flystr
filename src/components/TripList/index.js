8a99fa78e85deec880c929d847e1de73647c8cff
Sam Maaß <sam-github@maass.co>
2018-01-13 14:13:57 +0100

    add empty state for destinations

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types';
 import TripList from './component';
 import { getUserTrips } from '../../actions/tripActions';
 import { connect } from 'react-redux';
+import EmptyState from '../EmptyState';
+import PindropIcon from 'material-ui-icons/PinDrop';
 
 class TripListContainer extends Component {
   componentDidMount() {
     this.props.getUserTrips();
   }
   render() {
-    return <TripList trips={this.props.trips} />;
+    if (this.props.trips.length === 0) {
+      return (
+        <EmptyState title="Add your first destination" icon={<PindropIcon />} />
+      );
+    } else {
+      return <TripList trips={this.props.trips} />;
+    }
   }
 }
 
