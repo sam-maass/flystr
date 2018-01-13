5b98fc7ead5326fe0db61a0ef4423f71b6cdbce5
Sam Maaß <sam-github@maass.co>
2018-01-13 13:13:01 +0100

    redesign flights list

--- src/components/TripList/component.js
+++ src/components/TripList/component.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TripRow from '../TripRow';
+import withStyles from 'material-ui/styles/withStyles';
 
-const TripList = ({ trips = [] }) => {
-  return <div>{trips.map((item, key) => <TripRow key={key} {...item} />)}</div>;
+const TripList = ({ trips = [], classes }) => {
+  return (
+    <div className={classes.listWrapper}>
+      {trips.map((item, key) => <TripRow key={key} {...item} />)}
+    </div>
+  );
 };
 
-TripList.propTypes = { trips: PropTypes.array };
+TripList.propTypes = { trips: PropTypes.array, classes: PropTypes.object };
 
-export default TripList;
+const styles = {
+  listWrapper: {
+    marginTop: 8,
+    display: 'grid',
+    gridRowGap: '8px'
+  }
+};
+
+export default withStyles(styles)(TripList);
--- src/pages/Destinations.js
+++ src/pages/Destinations.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import TripList from '../components/TripList';
 import { Link } from 'react-router-dom';
+import AddIcon from 'material-ui-icons/Add';
+import Button from 'material-ui/Button/Button';
+import withStyles from 'material-ui/styles/withStyles';
 
-const DestinationsPage = () => {
+const DestinationsPage = ({ classes }) => {
   return (
     <div>
-      <h1>Welcome back, user</h1>
       <TripList />
-      <Link to="/new-trip"> Add another trip </Link>
+      <Link to="/new-trip">
+        <Button fab color="primary" aria-label="add" className={classes.fab}>
+          <AddIcon />
+        </Button>
+      </Link>
     </div>
   );
 };
 
-export default DestinationsPage;
+const styles = {
+  fab: {
+    position: 'fixed',
+    bottom: 16,
+    right: 16
+  }
+};
+
+export default withStyles(styles)(DestinationsPage);
