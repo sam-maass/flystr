3c2728b87f8e5954d31bea0bc9ffb677a4b8bdeb
Sam Maaß <sam-github@maass.co>
2018-01-13 14:19:30 +0100

    remove styled components

--- src/pages/NewTrip.js
+++ src/pages/NewTrip.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Container, Heading1 } from '../style-components';
 import NewTripForm from '../components/NewTripForm';
 
 const NewTrip = () => {
   return (
     <div>
-      <Container>
-        <Heading1>Add a trip</Heading1>
-        <NewTripForm />
-      </Container>
+      <NewTripForm />
     </div>
   );
 };
