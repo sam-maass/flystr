a6106a9b51c90d56bd94a0768941bd234bb6a72e
Sam Maaß <sam-github@maass.co>
2018-01-14 18:36:59 +0100

    add new deal form

---
+++ src/components/NewDealForm/component.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'formik';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import withStyles from 'material-ui/styles/withStyles';
+
+const FormikTextField = ({
+  elemKey,
+  type = 'text',
+  label,
+  defaultValue,
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur
+}) => {
+  return (
+    <TextField
+      name={elemKey}
+      label={label}
+      defaultValue={defaultValue}
+      type={type}
+      value={values[elemKey] || defaultValue}
+      error={touched[elemKey] && !!errors[elemKey]}
+      helperText={touched[elemKey] && errors[elemKey]}
+      onChange={handleChange}
+      onBlur={handleBlur}
+    />
+  );
+};
+
+FormikTextField.propTypes = {
+  elemKey: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  values: PropTypes.object,
+  defaultValue: PropTypes.object,
+  errors: PropTypes.object,
+  touched: PropTypes.object,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func
+};
+
+const NewDealForm = props => {
+  return (
+    <Form className={props.classes.form}>
+      <FormikTextField elemKey="origins" label="From" {...props} />
+      <FormikTextField elemKey="destinations" label="To" {...props} />
+      <FormikTextField
+        elemKey="startDate"
+        type="date"
+        label="When"
+        {...props}
+      />
+      <FormikTextField
+        elemKey="endDate"
+        type="date"
+        label="Returning on"
+        {...props}
+      />
+      <FormikTextField elemKey="price" type="number" label="Price" {...props} />
+      <FormikTextField elemKey="link" label="Link" {...props} />
+      <Button raised color="primary" onClick={props.handleSubmit}>
+        Add deal
+      </Button>
+    </Form>
+  );
+};
+
+NewDealForm.propTypes = {
+  classes: PropTypes.object,
+  values: PropTypes.object,
+  errors: PropTypes.object,
+  touched: PropTypes.object,
+  handleSubmit: PropTypes.func
+};
+
+const styles = {
+  form: {
+    margin: 8,
+    display: 'grid',
+    gridRowGap: '16px'
+  }
+};
+
+export default withStyles(styles)(NewDealForm);
---
+++ src/components/NewDealForm/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { withFormik } from 'formik';
+import { connect } from 'react-redux';
+import NewTripForm from './component';
+import Yup from 'yup';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import { apiUrl } from '../../settings';
+import moment from 'moment';
+
+const mapStateToProps = store => {
+  return {
+    user: store.user
+  };
+};
+
+const formikSettings = {
+  mapPropsToValues: () => {
+    return {
+      destinations: '',
+      origins: '',
+      startDate: moment().format('YYYY-MM-DD'),
+      endDate: moment()
+        .add(4, 'weeks')
+        .format('YYYY-MM-DD'),
+      price: '',
+      link: ''
+    };
+  },
+  validationSchema: Yup.object().shape({
+    destinations: Yup.string()
+      .min(3)
+      .required(),
+    origins: Yup.string()
+      .min(3)
+      .required(),
+    startDate: Yup.string().required(),
+    endDate: Yup.string().required(),
+    price: Yup.number()
+      .positive()
+      .required()
+  }),
+  handleSubmit: async (values, { setStatus }) => {
+    console.log(values);
+    await axios.post(`${apiUrl}/deal`, {
+      ...values
+    });
+    setStatus('done');
+  }
+};
+
+const RoutingWrapper = props => {
+  const isSubmitted = props.status === 'done';
+  if (isSubmitted) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/destinations'
+        }}
+      />
+    );
+  } else {
+    return <NewTripForm {...props} />;
+  }
+};
+
+const FormikForm = withFormik(formikSettings)(RoutingWrapper);
+const DealFormContainer = connect(mapStateToProps)(FormikForm);
+export default DealFormContainer;
