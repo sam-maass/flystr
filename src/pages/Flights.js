76ba1070d85ea0c789afcb9e065bfc2938784c95
Sam Maaß <sam-github@maass.co>
2018-01-15 21:55:53 +0100

    fix(UI): use wrapper div on FlightsPage

--- src/pages/Flights.js
+++ src/pages/Flights.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import DealList from '../components/DealList';
+import withStyles from 'material-ui/styles/withStyles';
+import PropTypes from 'prop-types';
 
-const FlightsPage = () => {
-  return <DealList allUserDeals />;
+const FlightsPage = ({ classes }) => {
+  return (
+    <div className={classes.pageContainer}>
+      <DealList allUserDeals />
+    </div>
+  );
 };
 
-export default FlightsPage;
+FlightsPage.propTypes = {
+  classes: PropTypes.object
+};
+
+const styles = {
+  pageContainer: {
+    marginRight: 8,
+    marginLeft: 8
+  }
+};
+
+export default withStyles(styles)(FlightsPage);
