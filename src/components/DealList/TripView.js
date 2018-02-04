f20342435936523848a14dbc47916f6bdbc46e8d
Sam Maaß <sam-github@maass.co>
2018-02-04 16:49:00 +0100

    remove unused var

--- src/components/DealList/TripView.js
+++ src/components/DealList/TripView.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'material-ui/Card/Card';
 import CardContent from 'material-ui/Card/CardContent';
