7b711b1540e338795a3d6cf37f9820a5480449f0
Sam Maass <sam-github@maass.co>
2018-07-01 14:52:37 +0000

    feat: add HugeHeadline component

---
+++ src/components/HugeHeadline/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { css } from 'emotion';
+import PropTypes from 'prop-types';
+
+const container = css`
+  width: 100%;
+`;
+
+const typography = css`
+  font-family: 'Comfortaa';
+  font-style: normal;
+  font-weight: bold;
+  line-height: 50px;
+  font-size: 36px;
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  color: #fafafa;
+`;
+const bar = css`
+  height: 5px;
+  width: 200px;
+  margin: auto;
+  background-color: #ff6d00;
+`;
+
+const HugeHeadline = ({ title, withBar }) => {
+  return (
+    <div className={container}>
+      <div className={typography}>{title}</div>
+      {withBar && <div className={bar} />}
+    </div>
+  );
+};
+
+HugeHeadline.propTypes = {
+  title: PropTypes.string,
+  withBar: PropTypes.bool
+};
+
+export default HugeHeadline;
