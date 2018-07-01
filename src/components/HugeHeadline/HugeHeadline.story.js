bbecccfbcb2c79d0cb6539cbc2adaf18f928a785
Sam Maass <sam-github@maass.co>
2018-07-01 14:58:32 +0000

    fix: remove false import

--- src/components/HugeHeadline/HugeHeadline.story.js
+++ src/components/HugeHeadline/HugeHeadline.story.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import HugeHeadline from '.';
-import * as emotion from 'emotion';
-import { createSerializer } from 'jest-emotion';
-
-expect.addSnapshotSerializer(createSerializer(emotion));
 
 storiesOf('HugeHeadline', module).add('default', () => (
   <HugeHeadline title="I'm a huge headline" />
