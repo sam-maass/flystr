55423fdf86efecbb87d938936ec384c9bc3def09
Sam Maass <sam-github@maass.co>
2018-07-02 07:39:40 +0000

    fix: add mocks and shallow rendering to jest

--- jestSetup.js
+++ jestSetup.js
@@ -1,4 +1,7 @@
 import * as emotion from 'emotion'
 import { createSerializer } from 'jest-emotion'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 expect.addSnapshotSerializer(createSerializer(emotion))
+configure({ adapter: new Adapter() });
