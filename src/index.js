f4f641c7c49c07f9eb4503ca937094103b2cf4db
Sam Maass <sam-github@maass.co>
2018-07-02 21:10:47 +0000

    fix: use mui theme for styling mui elements

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import store from './store';
 import LayoutContainer from './Layout';
 import registerServiceWorker from './registerServiceWorker';
 import './injectGlobalStyles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { styles } from './styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: styles.colors.blue3 },
+    secondary: { main: styles.colors.orange },
+    typography: {
+      fontFamily: ['Open Sans', 'Roboto', 'sans-serif'].join(',')
+    }
+  }
+});
 
 ReactDOM.render(
   <Provider store={store}>
-    <LayoutContainer />
+    <MuiThemeProvider theme={theme}>
+      <LayoutContainer />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
