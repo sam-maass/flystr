0096ceced0e6a5ade514dac4f4cee70011d708d6
Sam <sam@pop-os.localdomain>
2018-07-22 17:32:56 +0200

    mod: move FormikTextField to own file

---
+++ src/components/FormikTextField/index.js
@@ -0,0 +1,47 @@
+import TextField from '@material-ui/core/TextField';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FormikTextField = ({
+  elemKey,
+  type = 'text',
+  label,
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  placeholder,
+  InputProps
+}) => {
+  return (
+    <TextField
+      name={elemKey}
+      label={label}
+      type={type}
+      value={values[elemKey]}
+      placeholder={placeholder}
+      error={touched[elemKey] && Boolean(errors[elemKey])}
+      helperText={touched[elemKey] && errors[elemKey]}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      InputProps={InputProps}
+    />
+  );
+};
+
+FormikTextField.propTypes = {
+  elemKey: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  values: PropTypes.object,
+  defaultValue: PropTypes.any,
+  errors: PropTypes.object,
+  touched: PropTypes.object,
+  handleChange: PropTypes.func,
+  handleBlur: PropTypes.func,
+  InputProps: PropTypes.object
+};
+
+export default FormikTextField;
