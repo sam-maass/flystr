e949a64d06bc12c95214241a66a751a7d66de47d
Sam <sam@pop-os.localdomain>
2018-07-22 17:46:44 +0200

    feat: add DateSelection page

---
+++ src/components/DateSelection/DateSelection.story.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import DateSelection from '.';
+
+storiesOf('DateSelection', module).add('default', () => <DateSelection />);
