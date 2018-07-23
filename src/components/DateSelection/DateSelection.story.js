4c9ecb9b41e462f8be34b115e83cb76bfcbf21a9
Sam <sam@pop-os.localdomain>
2018-07-23 15:06:51 +0200

    mod: remove DateSelection

--- src/components/DateSelection/DateSelection.story.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import DateSelection from '.';
-
-storiesOf('DateSelection', module).add('default', () => <DateSelection />);
