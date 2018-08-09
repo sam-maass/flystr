82663556513cea5b7bd9ede126648f3532796a43
Sam <sam@pop-os.localdomain>
2018-08-09 18:58:39 +0200

    feat: create snapshots during deployment

---
+++ prep.js
@@ -0,0 +1,14 @@
+exports.default = {
+  routes: ['/'],
+  timeout: 5000,
+  dimensions: {
+    width: 1440,
+    height: 900
+  },
+  https: true,
+  hostname: 'https://flystr.com',
+  useragent: 'Prep',
+  minify: false,
+  concurrency: 4,
+  additionalSitemapUrls: []
+};
