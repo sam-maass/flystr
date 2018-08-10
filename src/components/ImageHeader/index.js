e14b95e6ef0def3210ce50e15f32325bf514e081
Sam <sam@pop-os.localdomain>
2018-08-10 18:04:51 +0200

    feat: update landing page

--- src/components/ImageHeader/index.js
+++ src/components/ImageHeader/index.js
@@ -10,31 +10,55 @@ const header = css`
   background-repeat: no-repeat;
   background-position-y: 50%;
   padding: 10px;
-`;
-
-const logoStyle = css`
-  width: 150px;
-  height: 50px;
-  margin: auto;
-  background-image: url('/logo.svg');
-  background-size: contain;
-  background-repeat: no-repeat;
-  margin-bottom: 20px;
+  display: grid;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  grid-template-rows: 50px 1fr 50px;
+  grid-template-areas:
+    'logo'
+    'headline'
+    '.';
   @media only screen and (min-width: 1024px) {
-    width: 300px;
-    margin-left: 0;
+    grid-template-columns: 150px 1fr 150px;
+    height: 30vw;
+    max-height: 33vh;
+    grid-template-areas:
+      'logo . .'
+      '. headline .'
+      '. . .';
+  }
+  .headline {
+    grid-area: headline;
+    justify-self: center;
+    align-self: center;
+  }
+  .logo {
+    grid-area: logo;
+    width: 150px;
+    height: 50px;
+    margin: auto;
+    background-image: url('/logo.svg');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin-bottom: 20px;
+    @media only screen and (min-width: 1024px) {
+      width: 300px;
+      margin-left: 0;
+    }
   }
 `;
 
 const ImageHeader = () => {
   return (
     <div className={header}>
-      <div className={logoStyle} />
-      <HugeHeadline withBar>
-        select your destination
-        <br />
-        we find the best flights
-      </HugeHeadline>
+      <div className="logo" />
+      <div className="headline">
+        <HugeHeadline withBar>
+          select your destination
+          <br />
+          we find the best flights
+        </HugeHeadline>
+      </div>
     </div>
   );
 };
