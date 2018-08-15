3692ef280aa867988383369296e685bff352a498
Sam <sam@pop-os.localdomain>
2018-08-15 22:11:06 +0200

    feat: link signup on landingpage

--- src/components/BetaSignupForm/BetaSignupForm.story.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import BetaSignupForm from '.';
-
-storiesOf('BetaSignupForm', module).add('default', () => <BetaSignupForm />);
--- src/components/ImageHeader/ImageHeader.test.js
+++ src/components/ImageHeader/ImageHeader.test.js
@@ -1,9 +1,14 @@
 import renderer from 'react-test-renderer';
 import React from 'react';
 import ImageHeader from '.';
+import { MemoryRouter } from 'react-router';
 
 it('ImageHeader: default', () => {
-  const component = renderer.create(<ImageHeader />);
+  const component = renderer.create(
+    <MemoryRouter>
+      <ImageHeader />
+    </MemoryRouter>
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
