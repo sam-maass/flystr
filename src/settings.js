4578a086a3240e6be52cf37e806514c53486697d
Sam <sam@pop-os.localdomain>
2018-09-15 20:54:04 +0200

    fix: load edit page correctly

--- src/settings.js
+++ src/settings.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const isProd = process.env.NODE_ENV === 'production';
 const apiUrl = isProd ? 'https://api.flystr.com' : 'http://localhost:3000';
-const isBrowser = typeof window !== 'undefined';
+export const isBrowser = () => typeof window !== 'undefined';
 
 const getToken = () =>
-  isBrowser ? window.localStorage.getItem('currentJWT') : null;
+  isBrowser() ? window.localStorage.getItem('currentJWT') : null;
 export const api = () => {
   return axios.create({
     baseURL: apiUrl,
