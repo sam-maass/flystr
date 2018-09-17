2cab05334645760c0dbb9cc0596a324a8d4aa7c8
Sam <sam@pop-os.localdomain>
2018-09-17 14:47:33 +0200

    feat: move destination images to own service closes #33

--- src/getDestinationImage.js
+++ src/getDestinationImage.js
@@ -1,26 +1,9 @@
-import hash from 'string-hash';
-const fallbackImages = ['airplane', 'airplane2', 'airplane3', 'airplane4'];
-
-const getFallback = code => {
-  const index = hash(code) % fallbackImages.length;
-  return fallbackImages[index];
-};
-
-export const getDestinationImage = (type, code, { fallback } = {}) => {
+export const getDestinationImage = (type, code = {}) => {
   switch (type) {
     case 'header':
-      if (!fallback) {
-        return `/images/header/${code}.jpg`;
-      } else {
-        return `/images/header/${getFallback(code)}.jpg`;
-      }
+      return `https://flystr.com/destination-images/header/${code}.jpg`;
     case 'square':
-      if (!fallback) {
-        return `/images/square/${code}.jpg`;
-      } else {
-        return `/images/square/${getFallback(code)}.jpg`;
-      }
-
+      return `https://flystr.com/destination-images/square/${code}.jpg`;
     default:
       break;
   }
