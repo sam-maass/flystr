4cec7053b9232312367ea23ed8a107f80d7db74b
Sam <sam@pop-os.localdomain>
2018-11-19 19:08:22 +0100

    feat-update-service-worker

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -15,6 +15,30 @@ self.addEventListener('push', e => {
   e.waitUntil(evaluateNotification(e));
 });
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('cache').then(cache => {
+      return cache.addAll(['/deals', '/manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(async () => {
+      //Check to see if you have it in the cache
+      //Return response
+      //If not in the cache, then return error page
+      const cache = await caches.open('cache');
+      const matching = await cache.match(event.request);
+      const report =
+        !matching || matching.status === 404
+          ? Promise.reject('no-match')
+          : matching;
+      return report;
+    })
+  );
+});
 async function evaluateNotification(e) {
   let body;
   if (e.data) {
--- src/updateServiceWorker.js
+++ src/updateServiceWorker.js
@@ -1,7 +1,9 @@
 export const updateServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const reg = await navigator.serviceWorker.getRegistration();
+      const reg = await navigator.serviceWorker.register(
+        'custom-service-worker.js'
+      );
       reg && reg.update();
     } catch (error) {
       // eslint-disable-next-line no-console
