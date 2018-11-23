7697143d3e4622e530e3198701a52a5dcd2b8bec
Sam Maass <sam@maass.co>
2018-11-23 09:38:47 +0100

    fix: login on ssr (#130)

--- src/components/GoogleButton/component.js
+++ src/components/GoogleButton/component.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
-import GoogleLogin /*GoogleLogout*/ from 'react-google-login';
+const { GoogleLogin } =
+  typeof window === 'object' ? require('react-google-login') : {};
 
 export const GoogleButtonComponent = ({
-  loggedIn,
   onLoginFailure,
   onLoginSuccess,
   text
 }) => {
-  if (!loggedIn) {
-    return (
-      <GoogleLogin
-        clientId="1059931024943-1u64m1fh6glpodhalllbkbul1hbsdbfh.apps.googleusercontent.com"
-        style={{ width: '100%' }}
-        render={renderProps => (
-          <Button
-            onClick={renderProps.onClick}
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            {text}
-          </Button>
-        )}
-        onSuccess={onLoginSuccess}
-        onFailure={onLoginFailure}
-      />
-    );
-  } else {
-    return null;
-  }
+  return (
+    <GoogleLogin
+      clientId="1059931024943-1u64m1fh6glpodhalllbkbul1hbsdbfh.apps.googleusercontent.com"
+      style={{ width: '100%' }}
+      render={renderProps => (
+        <Button
+          onClick={renderProps.onClick}
+          fullWidth
+          variant="contained"
+          color="primary"
+        >
+          {text}
+        </Button>
+      )}
+      onSuccess={onLoginSuccess}
+      onFailure={onLoginFailure}
+    />
+  );
 };
 
 GoogleButtonComponent.propTypes = {
