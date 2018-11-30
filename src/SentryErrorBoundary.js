f03c26ec5e2f0ad4b9187950fbef7160aad1d0ff
Sam Maass <sam@maass.co>
2018-11-30 22:50:15 +0100

    feat: add sentry (#147)
    * feat: add sentry
    
    * add href

---
+++ src/SentryErrorBoundary.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as Sentry from '@sentry/browser';
+
+export class SentryErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({ error });
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key]);
+      });
+      Sentry.captureException(error);
+    });
+  }
+
+  render() {
+    if (this.state.error) {
+      //render fallback UI
+      return (
+        <a href="#feedback" onClick={() => Sentry.showReportDialog()}>
+          Report feedback
+        </a>
+      );
+    } else {
+      //when there's not an error, render children untouched
+      // eslint-disable-next-line react/prop-types
+      return this.props.children;
+    }
+  }
+}
