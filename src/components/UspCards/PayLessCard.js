c8eda9d3cf38097072e448ee565642d420bb1518
Sam <sam@pop-os.localdomain>
2018-12-04 18:51:51 +0100

    feat: add value cards

--- src/components/UspCards/PayLessCard.js
+++ src/components/UspCards/PayLessCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { css } from 'emotion';
 import Card from '@material-ui/core/Card';
-import { Link } from 'react-router-dom';
 import { styles, classes } from '../../styles';
 import { logClick } from '../../utils/logClick';
+import { ModalLink } from '../ModalLink';
 
 const style = css`
   width: 100%;
@@ -11,6 +11,15 @@ const style = css`
   a {
     text-decoration: none;
   }
+  border-radius: 4px;
+  transition: all 0.15s;
+
+  :hover {
+    filter: brightness(1.15);
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.32), 0 2px 2px 0 rgba(0, 0, 0, 0.16),
+      0 0 0 1px rgba(0, 0, 0, 0.08);
+  }
+
   .card-content {
     background-image: linear-gradient(to right top, #48c6ef, #6f86d6);
   }
@@ -21,7 +30,7 @@ const style = css`
     background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2));
   }
   .text {
-    margin: 24px 8px;
+    margin: 8px;
     display: grid;
     align-items: center;
     justify-items: center;
@@ -31,40 +40,38 @@ const style = css`
   }
   h3 {
     width: 90%;
+    line-height: 0;
     .dashed {
       font-weight: bold;
       padding: 4px;
-      line-height: 2;
-      border-bottom: 3px dashed ${styles.colors.white};
+      border-bottom: 2px dashed ${styles.colors.white};
     }
   }
 `;
 
-const FilterDealsCard = () => {
+const PayLessCard = () => {
   return (
     <div className={style}>
-      <Link
-        to="/trips"
-        onClick={logClick('/trips', { category: 'Deals | Filter Deals Card' })}
+      <ModalLink
+        modal="signup"
+        onClick={logClick('/trips', { category: 'Deals | Pay Less Card' })}
       >
         <Card>
           <div className="card-content">
             <div className="backdrop">
               <div className="text">
-                <span>Looking for other destinations?</span>
+                <span>Never miss a cheap flight</span>
+                <img src="/best-price-card.svg" alt="Pricing" />
                 <h3>
-                  <span className="dashed">
-                    1. Set up your dream trip <br />
-                    2. We find the best deal
-                  </span>
+                  <span className="dashed">Pay up to 67% less</span>
                 </h3>
               </div>
             </div>
           </div>
         </Card>
-      </Link>
+      </ModalLink>
     </div>
   );
 };
 
-export default FilterDealsCard;
+export { PayLessCard };
