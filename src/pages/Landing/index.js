2fc7459c0b8aa0df114f0e70f565f83164f74d72
Your Name <you@example.com>
2018-12-19 15:27:54 +0100

    add h1 to landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -28,19 +28,19 @@ class Landing extends React.Component {
     return (
       <div>
         <Helmet>
-          <title>All Cheap flights in one place | Tripfixed</title>
+          <title>Find The Cheapest Flights With Ease - Tripfixed</title>
           <meta
             name="description"
-            content="Tripfixed makes cheap trips easy. Save up to 67% on flights and get notifications so you never miss a cheap flight again."
+            content="Find cheap flights to your favorite vacation destinations. Save up to 67% and get notifications so you never miss a bargain flight again."
           />
         </Helmet>
         <ImageHeader />
 
         <div className={style}>
           <section>
-            <h2 className="h2-text text-center">
-              Find cheap travel destinations with ease
-            </h2>
+            <h1 className="h2-text text-center">
+              Discover cheap flight destinations with ease
+            </h1>
             <div className="grid">
               <div className="advantage-box-container">
                 <AdvantageBox heading="Stay Flexible">
